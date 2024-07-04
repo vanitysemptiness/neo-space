1593c8b93ac80b75aedcef983902cbed5c9c782b
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-07-04 19:59:37 -0400

    :

--- src/user_action_mode.rs
+++ src/user_action_mode.rs
@@ -1,8 +1,13 @@
 use macroquad::{
-    color::BLACK, input::{is_mouse_button_down, mouse_position, MouseButton}, math::Vec2, shapes::draw_circle
+    input::{is_mouse_button_down, is_mouse_button_pressed, mouse_position, MouseButton},
+    math::Vec2,
+    shapes::draw_circle,
 };
 
-use crate::{camera::Camera, canvas_state::{CanvasState, DrawnPoint}};
+use crate::{
+    camera::Camera,
+    canvas_state::{CanvasState, DrawnPoint},
+};
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum UserActionMode {
@@ -11,10 +16,15 @@ pub enum UserActionMode {
     Erase,
 }
 
-pub fn observe_user_action(camera: &mut Camera, mode: &UserActionMode, mut state: CanvasState) -> CanvasState {
+pub fn observe_user_action(
+    camera: &mut Camera,
+    mode: &UserActionMode,
+    mut state: CanvasState,
+) -> CanvasState {
     match mode {
         UserActionMode::Grab => {
-            let (is_dragging, last_mouse_position) = handle_dragging(camera, state.is_dragging, state.last_mouse_position);
+            let (is_dragging, last_mouse_position) =
+                handle_dragging(camera, state.is_dragging, state.last_mouse_position);
             state.is_dragging = is_dragging;
             state.last_mouse_position = last_mouse_position;
         }
@@ -49,29 +59,52 @@ pub fn handle_dragging(
 }
 
 fn handle_drawing(state: &mut CanvasState, camera: &Camera) {
-    if is_mouse_button_down(MouseButton::Left) {
-        let world_position = camera.screen_to_world(mouse_position().into());
+    let current_position = camera.screen_to_world(mouse_position().into());
+
+    if is_mouse_button_pressed(MouseButton::Left) {
+        // Start a new line segment
         state.drawn_points.push(DrawnPoint {
-            position: world_position,
-            color: BLACK,
-            size: 2.0, // Default size, can be adjusted later
+            position: current_position,
+            color: state.current_color,
+            size: state.current_size,
         });
+    } else if is_mouse_button_down(MouseButton::Left) {
+        if let Some(last_point) = state.drawn_points.last() {
+            let last_position = last_point.position;
+            let distance = Vec2::distance(last_position, current_position);
+            let num_points = (distance / (state.current_size * 0.5)).ceil() as usize;
+
+            for i in 1..=num_points {
+                let t = i as f32 / num_points as f32;
+                let interpolated_position = last_position.lerp(current_position, t);
+                state.drawn_points.push(DrawnPoint {
+                    position: interpolated_position,
+                    color: state.current_color,
+                    size: state.current_size,
+                });
+            }
+        }
     }
 }
 
 fn handle_erasing(state: &mut CanvasState, camera: &Camera) {
     if is_mouse_button_down(MouseButton::Left) {
         let world_position = camera.screen_to_world(mouse_position().into());
-        let erase_radius = 10.0; // Default size, can be adjusted later
-        state.drawn_points.retain(|point| {
-            Vec2::distance(point.position, world_position) > erase_radius
-        });
+        let erase_radius = state.current_size * 2.0; // Adjust erase radius based on current size
+        state
+            .drawn_points
+            .retain(|point| Vec2::distance(point.position, world_position) > erase_radius);
     }
 }
 
 pub fn draw_canvas(state: &CanvasState, camera: &Camera) {
     for point in &state.drawn_points {
         let screen_position = camera.world_to_screen(point.position);
-        draw_circle(screen_position.x, screen_position.y, point.size * camera.zoom, point.color);
+        draw_circle(
+            screen_position.x,
+            screen_position.y,
+            point.size * camera.zoom,
+            point.color,
+        );
     }
 }
